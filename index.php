3efbfd824f5e234b4e4a5d421253fa80b24b226c
psyb <psyb@users.noreply.github.com>
2016-10-18 14:48:56 +0200

    Practice Standard Php Class with properties and functions

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-    
+<?php 
+
+
+public class ClassName 
+{
+
+	//fields
+	$properties = array();
+
+
+
+
+
+	//Propterties
+	public function __get($properties)
+	{
+		if (condition) {
+			# code...
+		}
+	}
+	
+
+	//Constructur
+	function __construct(argument)
+	{
+		# code...
+	}
+
+
+
+	public function Method1()
+	{
+				
+	}
+}
+
+?>
